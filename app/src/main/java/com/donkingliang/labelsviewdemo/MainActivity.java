b24c97f65812a8f849c9f3f782079aa6923427ca
donkingliang <1043214265@qq.com>
2017-04-26 19:40:41 +0800

    添加Demo

---
+++ app/src/main/java/com/donkingliang/labelsviewdemo/MainActivity.java
@@ -0,0 +1,84 @@
+package com.donkingliang.labelsviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.donkingliang.labels.LabelsView;
+
+import java.util.ArrayList;
+
+import static com.donkingliang.labelsviewdemo.R.id.labels;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LabelsView labelsView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        labelsView = (LabelsView) findViewById(labels);
+
+        //测试的数据
+        ArrayList<String> label = new ArrayList<>();
+        label.add("Android");
+        label.add("IOS");
+        label.add("前端");
+        label.add("后台");
+        label.add("微信开发");
+        label.add("游戏开发");
+        label.add("Java");
+        label.add("JavaScript");
+        label.add("C++");
+        label.add("PHP");
+        label.add("Python");
+        label.add("Swift");
+        labelsView.setLabels(label);
+
+        findViewById(R.id.btn_none).setOnClickListener(this);
+        findViewById(R.id.btn_single).setOnClickListener(this);
+        findViewById(R.id.btn_multi).setOnClickListener(this);
+        findViewById(R.id.btn_multi_5).setOnClickListener(this);
+        findViewById(R.id.btn_un_select).setOnClickListener(this);
+        findViewById(R.id.btn_click).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        labelsView.setOnLabelClickListener(null);
+        switch (v.getId()) {
+            case R.id.btn_none:
+                labelsView.setSelectType(LabelsView.SelectType.NONE);
+                break;
+
+            case R.id.btn_single:
+                labelsView.setSelectType(LabelsView.SelectType.SINGLE);
+                break;
+
+            case R.id.btn_multi:
+                labelsView.setSelectType(LabelsView.SelectType.MULTI);
+                labelsView.setMaxSelect(0);
+                break;
+            case R.id.btn_multi_5:
+                labelsView.setSelectType(LabelsView.SelectType.MULTI);
+                labelsView.setMaxSelect(5);
+                break;
+            case R.id.btn_un_select:
+                labelsView.clearAllSelect();
+                break;
+            case R.id.btn_click:
+                labelsView.setSelectType(LabelsView.SelectType.NONE);
+                labelsView.setOnLabelClickListener(new LabelsView.OnLabelClickListener() {
+                    @Override
+                    public void onLabelClick(View label, String labelText, int position) {
+                        Toast.makeText(MainActivity.this, position + " : " + labelText,
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+                break;
+        }
+    }
+}
